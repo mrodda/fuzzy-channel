9ef4bc94a43dbf8a5ee768f37a68209ae97aeede
Michael Rodda <mrodda94@gmail.com>
2022-10-08 13:04:19 -0700

    Test Client.

---
+++ src/bin/server.rs
@@ -0,0 +1,22 @@
+use std::{
+    io::{Error, Read},
+    net::{TcpListener}
+};
+use log::{info, error};
+
+fn run() -> Result<(), Error> {
+    // Server.
+    let listener = TcpListener::bind("127.0.0.1:7878")?;
+    // Listen for a connection.
+    let (mut stream, _addr) = listener.accept()?;
+    // Read a string.
+    let mut buf = String::new();
+    let _size = stream.read_to_string(&mut buf)?;
+    info!("Read '{}'", buf);
+    Ok(())
+}
+fn main() {
+    if let Err(e) = run() {
+        error!("Error {:?}", e);
+    }
+}
