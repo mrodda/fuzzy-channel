02e77b815cdcee1199d509b0679c6cf2b6c867a3
Michael Rodda <mrodda94@gmail.com>
2022-10-22 16:51:27 -0700

    Missed adds

--- src/bin/test-client.rs
+++ src/bin/test-client.rs
@@ -1,3 +1,19 @@
+use std::{
+    io::{Error, Read, Write},
+    net::{TcpStream}
+};
+
+fn run() -> Result<(), Error> {
+    let mut client = TcpStream::connect("127.0.0.1:7878")?;
+    client.write_fmt(format_args!("Hi."))?;
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, client.");
+    println!("Start");
+    if let Err(e) = run() {
+        println!("Error: {:?}", e)
+    } else {
+        println!("End")
+    }
 }
